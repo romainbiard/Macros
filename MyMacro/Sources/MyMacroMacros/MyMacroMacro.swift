843961d412d4d4667be0d643348b8f04361aa7d9
Distiller <distiller@github.com>
2025-03-18 16:24:12 +0100

    init

---
+++ MyMacro/Sources/MyMacroMacros/MyMacroMacro.swift
@@ -0,0 +1,59 @@
+import SwiftCompilerPlugin
+import SwiftSyntax
+import SwiftSyntaxBuilder
+import SwiftSyntaxMacros
+
+/// Implementation of the `stringify` macro, which takes an expression
+/// of any type and produces a tuple containing the value of that expression
+/// and the source code that produced the value. For example
+///
+///     #stringify(x + y)
+///
+///  will expand to
+///
+///     (x + y, "x + y")
+public struct StringifyMacro: ExpressionMacro {
+    public static func expansion(
+        of node: some FreestandingMacroExpansionSyntax,
+        in context: some MacroExpansionContext
+    ) -> ExprSyntax {
+        guard let argument = node.arguments.first?.expression else {
+            fatalError("compiler bug: the macro does not have any arguments")
+        }
+
+        return "(\(argument), \(literal: argument.description))"
+    }
+}
+
+public struct ManagedTask: ExpressionMacro {
+  public static func expansion(of node: some SwiftSyntax.FreestandingMacroExpansionSyntax, in context: some SwiftSyntaxMacros.MacroExpansionContext) throws -> SwiftSyntax.ExprSyntax {
+    return "Task { /* body */ }"
+  }
+
+
+}
+
+public struct ManagedTaskBody: BodyMacro {
+  public static func expansion(
+    of node: AttributeSyntax,
+    providingBodyFor declaration: some DeclSyntaxProtocol & WithOptionalCodeBlockSyntax,
+    in context: some MacroExpansionContext
+  ) throws -> [CodeBlockItemSyntax] {
+    return []
+  }
+}
+
+public struct ManagingTask: MemberAttributeMacro {
+  public static func expansion(of node: SwiftSyntax.AttributeSyntax, attachedTo declaration: some SwiftSyntax.DeclGroupSyntax, providingAttributesFor member: some SwiftSyntax.DeclSyntaxProtocol, in context: some SwiftSyntaxMacros.MacroExpansionContext) throws -> [SwiftSyntax.AttributeSyntax] {
+    return []
+  }
+  
+
+}
+
+@main
+struct MyMacroPlugin: CompilerPlugin {
+    let providingMacros: [Macro.Type] = [
+      StringifyMacro.self, ManagedTask.self, ManagedTaskBody.self, ManagingTask.self
+    ]
+}
